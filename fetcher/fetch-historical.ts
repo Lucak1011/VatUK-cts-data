97ce5a1d3890eff7b1d14bfd0feb3748415fae28
Kristián Kunc <kristian.kunc@gmail.com>
2024-08-24 01:09:42 +0200

    fix running scripts

--- fetcher/fetch-historical.ts
+++ fetcher/fetch-historical.ts
@@ -1,12 +1,13 @@
+import { dumpToCsv } from "./csv.js";
 import { browserInit, browserKill, getMonth } from "./index.js";
+
 await browserInit();
 
 let data: any = [];
 for (let year = 2006; year <= new Date().getFullYear(); year++) {
 	for (let month = 1; month <= 12; month++) {
-		console.log(`Getting data for ${year}-${month}, data length: ${data.length}`);
-		data = [...data, ...(await getMonth(year, month))];
+		console.log(`Getting data for ${year}-${month}`);
+		dumpToCsv(await getMonth(year, month), `./data/${year}-${month}.csv`);
 	}
 }
 await browserKill();
-console.log(data.length);
--- fetcher/fetch-last-month.ts
+++ fetcher/fetch-last-month.ts
@@ -2,10 +2,7 @@ import { dumpToCsv } from "./csv.js";
 import { browserInit, browserKill, getMonth } from "./index.js";
 
 await browserInit();
-console.log(new Date().getFullYear(), new Date().getMonth());
 const newData = await getMonth(new Date().getFullYear(), new Date().getMonth());
 await browserKill();
 
-console.log(newData.length);
-console.log(newData);
 dumpToCsv(newData, `./data/${new Date().getFullYear()}-${new Date().getMonth()}.csv`);
